120ffcccafa7034c5c277f9e8e282c8dec0141dd
tash-2s <81064017+tash-2s@users.noreply.github.com>
2025-11-15 07:59:20 -0800

    py: refactor code

--- py/main.py
+++ py/main.py
@@ -2,37 +2,40 @@ from google import genai
 from google.genai import types
 from pydantic import BaseModel, Field
 from typing import List, Tuple
-
-
-class PointItem(BaseModel):
-    point: Tuple[int, int] = Field(
-        description="The normalized [y, x] coordinates, integers in the range 0–1000."
-    )
-    label: str = Field(description="A unique identifying label.")
-
-
-class OutputSchema(BaseModel):
-    points: List[PointItem] = Field(description="List of detected objects.")
-    steps: List[str] = Field(description="List of steps.")
+import textwrap
 
 
 def main():
+    json = plan()
+    print(json.model_dump_json(indent=2))
+
+
+def plan():
+    class PointItem(BaseModel):
+        point: Tuple[int, int] = Field(
+            description="The normalized [y, x] coordinates, integers in the range 0–1000."
+        )
+        label: str = Field(description="A unique identifying label.")
+
+    class OutputSchema(BaseModel):
+        points: List[PointItem] = Field(description="List of detected objects.")
+        steps: List[str] = Field(description="List of steps.")
+
     client = genai.Client()
 
     with open("data/1.png", "rb") as f:
         image_bytes = f.read()
 
-    prompt = """
-Our goal is to move objects (umbrellas, water bottles, glasses) in order to group similar items together.
+    prompt = textwrap.dedent("""\
+        Our goal is to move objects (umbrellas, water bottles, glasses) in order to group similar items together.
 
-Identify and point to relevant objects located near the center of the image.
-Each detected object must be assigned a unique identifying label, such as "green umbrella". The answer should follow the json format: [{"point": <point>, "label": <label1>}, ...]. The points are in [y, x] format normalized to 0-1000.
+        Identify and point to relevant objects located near the center of the image.
+        Each detected object must be assigned a unique identifying label, such as "green umbrella". The answer should follow the json format: [{"point": <point>, "label": <label1>}, ...]. The points are in [y, x] format normalized to 0-1000.
 
-Next, generate steps that describe the actions needed to achieve the goal.
-For example: "Move the sunglasses to the right side". The answer should follow the json format: [<step>, ...].
-"""
+        Next, generate steps that describe the actions needed to achieve the goal.
+        For example: "Move the sunglasses to the right side". The answer should follow the json format: [<step>, ...].""")
 
-    response = client.models.generate_content(
+    text = client.models.generate_content(
         model="gemini-robotics-er-1.5-preview",
         contents=[
             types.Part.from_bytes(
@@ -48,7 +51,11 @@ For example: "Move the sunglasses to the right side". The answer should follow t
         ),
     ).text
 
-    print(response)
+    assert text is not None
+
+    json = OutputSchema.model_validate_json(text)
+
+    return json
 
 
 if __name__ == "__main__":
