f1e67fb2cf650f270d649de7e865e3329c65ac06
tash-2s <81064017+tash-2s@users.noreply.github.com>
2025-11-15 08:47:27 -0800

    py: nanobanana

--- py/main.py
+++ py/main.py
@@ -3,11 +3,16 @@ from google.genai import types
 from pydantic import BaseModel, Field
 from typing import List, Tuple
 import textwrap
+from PIL import Image
+
+
+client = genai.Client()
 
 
 def main():
-    json = plan()
-    print(json.model_dump_json(indent=2))
+    # json = plan()
+    # print(json.model_dump_json(indent=2))
+    banana()
 
 
 def plan():
@@ -21,10 +26,7 @@ def plan():
         points: List[PointItem] = Field(description="List of detected objects.")
         steps: List[str] = Field(description="List of steps.")
 
-    client = genai.Client()
-
-    with open("data/1.png", "rb") as f:
-        image_bytes = f.read()
+    image = get_image_resized("data/1.png")
 
     prompt = textwrap.dedent("""\
         Our goal is to move objects (umbrellas, water bottles, glasses) in order to group similar items together.
@@ -38,14 +40,14 @@ def plan():
     text = client.models.generate_content(
         model="gemini-robotics-er-1.5-preview",
         contents=[
-            types.Part.from_bytes(
-                data=image_bytes,
-                mime_type="image/png",
-            ),
+            image,
             prompt,
         ],
         config=types.GenerateContentConfig(
             temperature=0.5,
+            thinking_config=types.ThinkingConfig(
+                thinking_budget=-1
+            ),  # dynamic thinking
             response_mime_type="application/json",
             response_json_schema=OutputSchema.model_json_schema(),
         ),
@@ -56,6 +58,37 @@ def plan():
     json = OutputSchema.model_validate_json(text)
 
     return json
+
+
+def banana():
+    prompt = textwrap.dedent("""\
+        Using the provided image, apply the following action to the scene: "Move the regular glasses next to the sunglasses."
+
+        Modify only what is necessary to perform this action. Keep all other elements of the image exactly the same.""")
+
+    image = get_image_resized("data/1.png")
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash-image",
+        contents=[prompt, image],
+        config=types.GenerateContentConfig(response_modalities=["Image"]),
+    )
+
+    assert response.parts is not None
+
+    for part in response.parts:
+        if part.inline_data is not None:
+            image = part.as_image()
+            assert image is not None
+            image.save("data/generated.png")
+
+
+def get_image_resized(img_path: str):
+    image = Image.open(img_path)
+    image = image.resize(
+        (800, int(800 * image.size[1] / image.size[0])), Image.Resampling.LANCZOS
+    )
+    return image
 
 
 if __name__ == "__main__":
