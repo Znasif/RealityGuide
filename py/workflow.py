37b8fbc8581f62ea4ff0fceb13ae4ae2c5e84827
tash-2s <81064017+tash-2s@users.noreply.github.com>
2025-11-16 05:55:56 -0800

    py: set thinking budget

--- py/workflow.py
+++ py/workflow.py
@@ -36,7 +36,7 @@ def generate_plan_from_image(image: Image.Image) -> WorkflowArtifacts:
         contents=[resized_image, PLAN_PROMPT],
         config=types.GenerateContentConfig(
             temperature=0.5,
-            thinking_config=types.ThinkingConfig(thinking_budget=-1),
+            thinking_config=types.ThinkingConfig(thinking_budget=1024),
             response_mime_type="application/json",
             response_json_schema=OutputSchema.model_json_schema(),
         ),
@@ -70,7 +70,7 @@ def refresh_plan_from_image(
         contents=[resized_image, completion_prompt],
         config=types.GenerateContentConfig(
             temperature=0.5,
-            thinking_config=types.ThinkingConfig(thinking_budget=-1),
+            thinking_config=types.ThinkingConfig(thinking_budget=512),
             response_mime_type="application/json",
             response_json_schema=OutputSchema.model_json_schema(),
         ),
